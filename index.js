71413f7cc5d5b1233d7972a4ab0aa9b9fe3e2893
“Daysha <+>
2022-02-05 11:29:15 -0800

    commit tree 8255bd44e1f2fbe68b61a89267bc698846e23318

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+   
+function map(array, f){
+    let newArray = [];
+    for(let a of array){
+      newArray.push(f(a));
+    }
+    return newArray
+}
+
+function reduce(src, cb, start){
+    let r = (!!start) ? start : src[0]
+    let i = (!!start) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
